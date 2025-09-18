a18faa42031bd0cccecd0a31abe06448037d91b2
Jireh <mecatejireh09@gmail.com>
2025-09-19 05:07:58 +0800

    27th

--- test-garden-a.js
+++
@@ -1,9 +0,0 @@
-// Simple test to verify Garden A data structure
-import { gardenAData } from './src/data/gardenA.js';
-
-console.log('Testing Garden A data structure...');
-console.log('Has data:', !!gardenAData);
-console.log('Has features:', !!gardenAData.features);
-console.log('Features count:', gardenAData.features?.length || 0);
-console.log('First feature:', gardenAData.features?.[0]);
-console.log('✅ Garden A data structure is valid!');
